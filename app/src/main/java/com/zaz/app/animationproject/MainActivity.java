8b18809ea4a09ec274ae4c52e1cdd2d30cab6430
ZhangYJie <1204133922@qq.com>
2019-06-06 08:38:09 +0800

    提交注释

--- app/src/main/java/com/zaz/app/animationproject/MainActivity.java
+++ app/src/main/java/com/zaz/app/animationproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.zaz.app.animationproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 测试自定义属性的activity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
