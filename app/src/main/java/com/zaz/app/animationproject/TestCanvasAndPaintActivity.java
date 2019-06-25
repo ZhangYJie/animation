68cb74ed12c38fc3a4526288f35a3486165caaac
ZhangYJie <1204133922@qq.com>
2019-06-25 14:42:49 +0800

    提交Canvas 和 Paint的测试

---
+++ app/src/main/java/com/zaz/app/animationproject/TestCanvasAndPaintActivity.java
@@ -0,0 +1,13 @@
+package com.zaz.app.animationproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestCanvasAndPaintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_canvas_and_paint);
+    }
+}
